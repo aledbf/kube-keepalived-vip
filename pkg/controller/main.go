f65d20d8cf7265a8c1dcdbcc9b23fbf8d21b3745
Mikhail Shaverdo <m.shaverdo@gmail.com>
2019-01-24 13:05:28 +0300

    Add flag to make ipvs reset optional
    * implement flag --release-vips default true. If it true, add --release-vips to keepalived args

--- pkg/controller/main.go
+++ pkg/controller/main.go
@@ -353,7 +353,7 @@ func (ipvsc *ipvsControllerController) Stop() error {
 }
 
 // NewIPVSController creates a new controller from the given config.
-func NewIPVSController(kubeClient *kubernetes.Clientset, namespace string, useUnicast bool, configMapName string, vrid int, proxyMode bool, iface string, httpPort int) *ipvsControllerController {
+func NewIPVSController(kubeClient *kubernetes.Clientset, namespace string, useUnicast bool, configMapName string, vrid int, proxyMode bool, iface string, httpPort int, releaseVips bool) *ipvsControllerController {
 	ipvsc := ipvsControllerController{
 		client:            kubeClient,
 		reloadRateLimiter: flowcontrol.NewTokenBucketRateLimiter(0.5, 1),
@@ -392,17 +392,18 @@ func NewIPVSController(kubeClient *kubernetes.Clientset, namespace string, useUn
 	iptInterface := utiliptables.New(execer, dbus, utiliptables.ProtocolIpv4)
 
 	ipvsc.keepalived = &keepalived{
-		iface:      iface,
-		ip:         nodeInfo.ip,
-		netmask:    nodeInfo.netmask,
-		nodes:      clusterNodes,
-		neighbors:  neighbors,
-		priority:   getNodePriority(nodeInfo.ip, clusterNodes),
-		useUnicast: useUnicast,
-		ipt:        iptInterface,
-		vrid:       vrid,
-		proxyMode:  proxyMode,
+		iface:       iface,
+		ip:          nodeInfo.ip,
+		netmask:     nodeInfo.netmask,
+		nodes:       clusterNodes,
+		neighbors:   neighbors,
+		priority:    getNodePriority(nodeInfo.ip, clusterNodes),
+		useUnicast:  useUnicast,
+		ipt:         iptInterface,
+		vrid:        vrid,
+		proxyMode:   proxyMode,
 		notify:     notify,
+		releaseVips: releaseVips,
 	}
 
 	ipvsc.syncQueue = task.NewTaskQueue(ipvsc.sync)
